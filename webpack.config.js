3d8f873082d3fe22f9a913a95bb33c12b9dfe506
Katrin Shibakova <katrina.gubanova@gmail.com>
2019-02-23 16:34:47 +0300

    module breakdown (part two)

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,9 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `sourse-map`,
+  devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    // publicPath: `http://localhost:8080/`,
     port: 8080,
     hot: true,
     compress: true
